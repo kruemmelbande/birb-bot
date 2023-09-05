0400db5b9a6345a6ad9dada05b48c025166cb60f
kruemmelbande <kai.scheufen@gmail.com>
2023-09-05 16:39:23 +0200

    Added automatic backups (that work for once)

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+date > uptime.log
+#backup old log, with date
+cp latest.log backups/$(date +%Y-%m-%d_%H.%M.%S).log
+#back up the database
+cp userdb.json backups/$(date +%Y-%m-%d_%H.%M.%S).json
 source ./env/bin/activate
-python bot.py
+#run the script, and save the log and errors
+python bot.py | tee latest.log 2>&1 
